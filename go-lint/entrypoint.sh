a451305bb3a66056d7f7d5d675b87cf7d069227d
Milad <moorara@users.noreply.github.com>
2024-04-04 17:06:18 -0400

    Housekeeping updates (#39)

--- go-lint/entrypoint.sh
+++ go-lint/entrypoint.sh
@@ -2,12 +2,15 @@
 
 set -eu
 
+# FIXME: https://github.com/actions/checkout/issues/1169
+git config --system --add safe.directory "$(pwd)"
+
 # Changing working directory to input path
 cd "$INPUT_PATH"
 
 # Run linters
 if [ -z "$INPUT_FROM_REVISION" ]; then
-  golangci-lint run --deadline="$INPUT_TIMEOUT" ./...
+  golangci-lint run --timeout="$INPUT_TIMEOUT" ./...
 else
-  golangci-lint run --deadline="$INPUT_TIMEOUT" --new-from-rev="$INPUT_FROM_REVISION" ./...
+  golangci-lint run --timeout="$INPUT_TIMEOUT" --new-from-rev="$INPUT_FROM_REVISION" ./...
 fi
