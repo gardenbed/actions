44b16801564d6933c9e6eb38020b59d274dc8bab
Milad <moorara@users.noreply.github.com>
2025-09-19 00:49:16 -0400

    Remove CodeClimate test reporter (#63)

--- go-cover/entrypoint.sh
+++ go-cover/entrypoint.sh
@@ -5,25 +5,12 @@ set -euo pipefail
 # Changing working directory to input path
 cd "$INPUT_PATH"
 
-# These two environment variables are read by Code Climate test reporter
-# See https://docs.codeclimate.com/docs/test-coverage-troubleshooting-branch-names
-# String operators: # deletes the shortest possible match from the left
-# See https://www.linuxjournal.com/article/8919
-export GIT_COMMIT_SHA="$GITHUB_SHA"
-export GIT_BRANCH="${GITHUB_REF#refs/heads/}"
-
-# Run if codeclimate_reporter_id input is set
-[ -n "$INPUT_CODECLIMATE_REPORTER_ID" ] && test-reporter before-build
-
 # Run go test command ...
 go test -race -cover -covermode=atomic -coverprofile=c.out ./...
 exit_code=$?
 
 # Run if codecov_token input is set
 [ -n "$INPUT_CODECOV_TOKEN" ] && codecov -f c.out -t "$INPUT_CODECOV_TOKEN"
-
-# Run if codeclimate_reporter_id input is set
-[ -n "$INPUT_CODECLIMATE_REPORTER_ID" ] && test-reporter after-build --id "$INPUT_CODECLIMATE_REPORTER_ID" --exit-code $exit_code --prefix "$(go list -m)"
 
 # Generate HTML coverage report
 go tool cover -html=c.out -o coverage.html
